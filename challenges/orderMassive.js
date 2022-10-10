c38591cfea66cc9ae7f49efe037c90c819f6e6d0
Malik Pro <32392757+MalikProtopopov@users.noreply.github.com>
2022-10-10 04:05:39 +0300

    Create orderMassive.js

---
+++ challenges/orderMassive.js
@@ -0,0 +1,16 @@
+// Состав смузи
+
+let liquids = ['вода', 'молоко', 'сок', 'чай', 'йогурт'];
+let fruits = ['киви', 'банан', 'персик', 'манго', 'груша', 'ананас'];
+let greens = ['мята', 'шпинат', 'руккола', 'петрушка', 'базилик'];
+
+// Выбор посетителя
+
+let chosenLiquid = 1;
+let chosenFruit = 3;
+let chosenGreen = 2;
+
+// Заказ
+
+
+let order ='Основа — ' +   liquids[chosenLiquid-1] + ', фрукт — ' + fruits[chosenFruit-1] + ', зелень — ' + greens[chosenGreen-1];
