527e4d28f5de41f6798798a9e3511cbac209b46f
Malik Pro <32392757+MalikProtopopov@users.noreply.github.com>
2022-10-10 04:20:52 +0300

    Create groceriesJoin.js

---
+++ challenges/groceriesJoin.js
@@ -0,0 +1,10 @@
+let groceries = ['чай', 'шпроты', 'печенье', 'сахар', 'чипсы'];
+let shoppingList = '';
+
+
+for (let i=0 ; i<groceries.length; i++) {
+
+
+  shoppingList =  groceries.join(', ') ;
+  
+  }
