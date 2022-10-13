e83da65c7d35799e0625ebfcab8d64311e772d8b
Malik Pro <32392757+MalikProtopopov@users.noreply.github.com>
2022-10-13 16:31:22 +0300

    Create matchingUsers.js

---
+++ challenges/matchingUsers.js
@@ -0,0 +1,10 @@
+let usersData = ['Виталий Иванович', 'Иннокентий Петрович', 'Александр Александрович', 'Игорь Олегович', 'Евгений Петрович', 'Игнат Денисович', 'Сергей Александрович', 'Семён Петрович'];
+let query = 'Александрович';
+let matchingUsers = 0;
+
+
+for (let i = 0; i<usersData.length; i++) {
+    if (usersData[i].indexOf(query)>=0) {
+      matchingUsers++
+  } 
+}
